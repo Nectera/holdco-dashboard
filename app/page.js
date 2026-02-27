e8b4b7bec868a0976f475173265ea9aebf4d26d7
Cody Gordon <cody@necteraholdings.com>
2026-02-26 22:11:12 -0700

    fix: add React import for Nora action cards

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Cell } from 'recharts'
 
 const renderMarkdown = (text) => {
